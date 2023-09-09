49da694fb7148b6634c730dd493d09b3c23ff329
redx70 <64176323+redx70@users.noreply.github.com>
2023-09-08 21:52:33 -0700

    e

--- dlvlistviewer/script.js
+++ dlvlistviewer/script.js
@@ -29,3 +29,19 @@ function unhoverImg2(img) {
 function redirect2() {
     window.location.replace("https://cornboar.com/dlvlistviewer/leaderboard/");
 }
+
+function hoverImg3(img) {
+    img.src = "https://cdn.discordapp.com/attachments/1040743145605976174/1149924630920970280/Screenshot_2023-09-08_212933.jpg";
+    img.style.maxHeight = "180px";
+    img.style.minHeight = "180px";
+}
+
+function unhoverImg3(img) {
+    img.src = "https://cdn.discordapp.com/attachments/1040743145605976174/1149925250692304907/Screenshot_2023-09-08_213207.jpg";
+    img.style.maxHeight = "150px";
+    img.style.minHeight = "150px";
+}
+
+function redirect3() {
+    window.location.replace("https://cornboar.com/dlvlistviewer/leaderboard/");
+}
