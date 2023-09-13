35355477fbd93289ffdb9fb4deb1a7e4185da906
redx70 <64176323+redx70@users.noreply.github.com>
2023-09-12 20:29:14 -0700

    ehhhhhhhhhhhhhhhhhhhhmazing

--- dlvlistviewer/script.js
+++ dlvlistviewer/script.js
@@ -1,13 +1,13 @@
 function hoverImg(img) {
     img.src = "https://cdn.discordapp.com/attachments/1040743145605976174/1149522075351007302/Screenshot_2023-09-07_184953.jpg";
-    img.style.maxHeight = "180px";
-    img.style.minHeight = "180px";
+    img.style.maxHeight = "300px";
+    img.style.minHeight = "300px";
 }
 
 function unhoverImg(img) {
     img.src = "https://cdn.discordapp.com/attachments/1040743145605976174/1149522075044814959/Screenshot_2023-09-07_184914.jpg";
-    img.style.maxHeight = "150px";
-    img.style.minHeight = "150px";
+    img.style.maxHeight = "250px";
+    img.style.minHeight = "250px";
 }
 
 function redirect() {
@@ -16,14 +16,14 @@ function redirect() {
 
 function hoverImg2(img) {
     img.src = "https://cdn.discordapp.com/attachments/1040743145605976174/1149539940326195210/image.png";
-    img.style.maxHeight = "180px";
-    img.style.minHeight = "180px";
+    img.style.maxHeight = "300px";
+    img.style.minHeight = "300px";
 }
 
 function unhoverImg2(img) {
     img.src = "https://cdn.discordapp.com/attachments/1040743145605976174/1149539940598829076/image.png";
-    img.style.maxHeight = "150px";
-    img.style.minHeight = "150px";
+    img.style.maxHeight = "250px";
+    img.style.minHeight = "250px";
 }
 
 function redirect2() {
@@ -32,14 +32,14 @@ function redirect2() {
 
 function hoverImg3(img) {
     img.src = "https://cdn.discordapp.com/attachments/1040743145605976174/1149924630920970280/Screenshot_2023-09-08_212933.jpg";
-    img.style.maxHeight = "180px";
-    img.style.minHeight = "180px";
+    img.style.maxHeight = "300px";
+    img.style.minHeight = "300px";
 }
 
 function unhoverImg3(img) {
     img.src = "https://cdn.discordapp.com/attachments/1040743145605976174/1149925250692304907/Screenshot_2023-09-08_213207.jpg";
-    img.style.maxHeight = "150px";
-    img.style.minHeight = "150px";
+    img.style.maxHeight = "250px";
+    img.style.minHeight = "250px";
 }
 
 function redirect3() {
