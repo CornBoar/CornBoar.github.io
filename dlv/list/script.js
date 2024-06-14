d1b724d0500d8a4d4ec2354081fa27b8ea405b2f
redx70 <64176323+redx70@users.noreply.github.com>
2024-06-13 20:16:20 -0700

    e

--- dlv/list/script.js
+++ dlv/list/script.js
@@ -66,7 +66,7 @@ fetch("https://user5e8e13639aafd2a.app.vtxhub.com/dlvlist/").then((Response) => 
         for (let i in data["main"]) {
           var formattedVictors = "";
           for (let e in Object.values(data["victors"][data["main"][i]])) {
-            e = Object.values(data["victors"][data["main"][i]])[e][0];
+            e = Object.values(data["victors"][data["main"][i]])[e][1];
             console.log(e, data["main"][i]);
             formattedVictors += `<div style="color:white" id="${data["main"][i].replace(/\s/g, "")}victors${Object.keys(data["victors"][data["main"][i]]).length}">${e}</div>`;
           }
