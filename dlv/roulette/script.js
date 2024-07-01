ab42b85800eaca4c3779546679967536bbeb74db
redx70 <64176323+redx70@users.noreply.github.com>
2024-07-01 16:29:54 -0700

    Fix

--- dlv/roulette/script.js
+++ dlv/roulette/script.js
@@ -6,10 +6,16 @@ fetch("https://api.github.com/repos/CornBoar/CornBoar.github.io/contents/api/dlv
         for (i in data["main"]) {
             positions[data["og_case"][data["main"][i]]] = data["main"].indexOf(data["main"][i]) + 1;
         }
-        document.getElementById("globalvar1").innerHTML = JSON.stringify(positions);
-        document.getElementById("globalvar2").innerHTML = JSON.stringify(data["colors"]);
+        globalvar1 = JSON.stringify(positions);
+        globalvar2 = JSON.stringify(data["colors"]);
         document.getElementById("startbutton").disabled = false;
 });
+
+var globalvar1 = "";
+var globalvar2 = "";
+var globalvar3 = "";
+var globalvar4 = "";
+var globalvar5 = "{}";
 
 function startButtonHover() {
     if (document.getElementById("startbutton").disabled === false) {
@@ -64,15 +70,15 @@ function giveUpButtonUnhover() {
 }
 
 function start() {
-    let positions = JSON.parse(document.getElementById("globalvar1").innerHTML);
+    let positions = JSON.parse(globalvar1);
     let levels = Object.keys(positions);
-    let colors = JSON.parse(document.getElementById("globalvar2").innerHTML);
+    let colors = JSON.parse(globalvar2);
     let level1 = levels[[Math.floor(Math.random() * levels.length)]];
-    document.getElementById("globalvar3").innerHTML = "1";
-    document.getElementById("globalvar4").innerHTML = colors[level1.toLowerCase()];
-    let usedLevelsDict = JSON.parse(document.getElementById("globalvar5").innerHTML);
+    globalvar3 = "1";
+    globalvar4 = colors[level1.toLowerCase()];
+    let usedLevelsDict = JSON.parse(globalvar5);
     usedLevelsDict[level1] = positions[level1];
-    document.getElementById("globalvar5").innerHTML = JSON.stringify(usedLevelsDict);
+    globalvar5 = JSON.stringify(usedLevelsDict);
     let level1html = `<div style="left: 50%; transform: translateX(-50%); border-radius: 25px; border: thick solid ${colors[level1.toLowerCase()]}; text-align: center; width: 500px; bottom: 135px; position: relative;">
     <h1 class="centered2" style="bottom: 15px; color: ${colors[level1.toLowerCase()]};">#${positions[level1]} - ${level1}</h1><input id="percentage" type="number" placeholder="At Least 1%" min="1" max="100" style="width: 100px; border-radius: 15px; height: 35px;position: relative; bottom: 50px;">
     <div id="buttons" style="text-align: center;">
@@ -95,20 +101,20 @@ function done() {
         document.getElementById("startbutton").disabled = false;
         alert("Unlucky Niggetron!");
     }
-    if (["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"].includes(document.getElementById("percentage").value) && parseInt(document.getElementById("percentage").value) >= parseInt(document.getElementById("globalvar3").innerHTML)) {
-        let positions = JSON.parse(document.getElementById("globalvar1").innerHTML);
+    if (["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"].includes(document.getElementById("percentage").value) && parseInt(document.getElementById("percentage").value) >= parseInt(globalvar3)) {
+        let positions = JSON.parse(globalvar1);
         let levels = Object.keys(positions);
-        let colors = JSON.parse(document.getElementById("globalvar2").innerHTML);
+        let colors = JSON.parse(globalvar2);
         let level = levels[[Math.floor(Math.random() * levels.length)]];
         let thingy = document.getElementById("percentage").value;
         if (window.location.href.includes("slugmode")) {
-            thingy = document.getElementById("globalvar3").innerHTML;
+            thingy = globalvar3;
         }
-        document.getElementById("globalvar3").innerHTML = parseInt(thingy) + 1;
+        globalvar3 = parseInt(thingy) + 1;
         document.getElementById("donebutton").remove();
         document.getElementById("giveupbutton").remove();
-        document.getElementById("percentage").outerHTML = `<h1 style="color: ${document.getElementById("globalvar4").innerHTML}; position: relative; bottom: 50px; font-family: 'Poppins', sans-serif; font-size: 50px; margin: 0px; border: 0px; padding: 0px;">${document.getElementById("percentage").value}%</h1>`;
-        let usedLevelsDict = JSON.parse(document.getElementById("globalvar5").innerHTML);
+        document.getElementById("percentage").outerHTML = `<h1 style="color: ${globalvar4}; position: relative; bottom: 50px; font-family: 'Poppins', sans-serif; font-size: 50px; margin: 0px; border: 0px; padding: 0px;">${document.getElementById("percentage").value}%</h1>`;
+        let usedLevelsDict = JSON.parse(globalvar5);
         if (Object.keys(usedLevelsDict).length === levels.length) {
             usedLevelsDict = {};
         }
@@ -116,19 +122,19 @@ function done() {
             level = levels[[Math.floor(Math.random() * levels.length)]];
         }
         usedLevelsDict[level] = positions[level];
-        document.getElementById("globalvar4").innerHTML = colors[level.toLowerCase()];
+        globalvar4 = colors[level.toLowerCase()];
         let html = `<div style="left: 50%; transform: translateX(-50%); border-radius: 25px; border: thick solid ${colors[level.toLowerCase()]}; text-align: center; width: 500px; bottom: 135px; position: relative;">
-        <h1 class="centered2" style="bottom: 15px; color: ${colors[level.toLowerCase()]};">#${positions[level]} - ${level}</h1><input id="percentage" type="number" placeholder="At Least ${parseInt(document.getElementById("globalvar3").innerHTML)}%" min="1" max="100" style="width: 100px; border-radius: 15px; height: 35px;position: relative; bottom: 50px;">
+        <h1 class="centered2" style="bottom: 15px; color: ${colors[level.toLowerCase()]};">#${positions[level]} - ${level}</h1><input id="percentage" type="number" placeholder="At Least ${parseInt(globalvar3)}%" min="1" max="100" style="width: 100px; border-radius: 15px; height: 35px;position: relative; bottom: 50px;">
         <div id="buttons" style="text-align: center;">
         <button id="donebutton" onclick=done() onmouseover=doneButtonHover() onmouseleave=doneButtonUnhover() style="background-color: lime; border-radius: 25px; width: 100px; height: 50px; position: relative; bottom: 35px; font-family: 'Poppins', sans-serif">Done</button>
         <button id="giveupbutton" onclick=giveUp() onmouseover=giveUpButtonHover() onmouseleave=giveUpButtonUnhover() style="background-color: red; border-radius: 25px; width: 100px; height: 50px; position: relative; bottom: 35px; font-family: 'Poppins', sans-serif">Give Up</button>
         </div>
         </div>`
-        if (parseInt(document.getElementById("globalvar3").innerHTML) > 99 || parseInt(thingy) + 1 > 99) {
-            alert(`You Have Completed The DLV Roulette With ${Object.keys(JSON.parse(document.getElementById("globalvar5").innerHTML)).length} ${Object.keys(JSON.parse(document.getElementById("globalvar5").innerHTML)).length > 1 ? "Levels" : "Level"}!`);
+        if (parseInt(globalvar3) > 99 || parseInt(thingy) + 1 > 99) {
+            alert(`You Have Completed The DLV Roulette With ${Object.keys(JSON.parse(globalvar5)).length} ${Object.keys(JSON.parse(globalvar5)).length > 1 ? "Levels" : "Level"}!`);
         }
         else {
-            document.getElementById("globalvar5").innerHTML = JSON.stringify(usedLevelsDict);
+            globalvar5 = JSON.stringify(usedLevelsDict);
             document.getElementById("levels").innerHTML += html;
         }
     }
@@ -136,7 +142,7 @@ function done() {
         if (parseInt(document.getElementById("percentage").value) > 100) {
             alert("Percentage Too High!");
         }
-        else if (parseInt(document.getElementById("percentage").value) < parseInt(document.getElementById("globalvar3").innerHTML)){
+        else if (parseInt(document.getElementById("percentage").value) < parseInt(globalvar3)){
             alert("Percentage Too Low!");
         }
     }
@@ -151,17 +157,17 @@ function reset() {
 
 function giveUp() {
     if (window.confirm("Are You Sure?") === true) {
-        if (window.confirm(`${parseInt(document.getElementById("globalvar3").innerHTML) - 1} Demons Completed! Show Remaining Demons?`)) {
+        if (window.confirm(`${parseInt(globalvar3) - 1} Demons Completed! Show Remaining Demons?`)) {
             document.getElementById("donebutton").remove();
             document.getElementById("giveupbutton").remove();
-            document.getElementById("percentage").outerHTML = `<h1 style="color: ${document.getElementById("globalvar4").innerHTML}; position: relative; bottom: 50px; font-family: 'Poppins', sans-serif; font-size: 50px; margin: 0px; border: 0px; padding: 0px;">${document.getElementById("globalvar3").innerHTML}%</h1>`;
+            document.getElementById("percentage").outerHTML = `<h1 style="color: ${globalvar4}; position: relative; bottom: 50px; font-family: 'Poppins', sans-serif; font-size: 50px; margin: 0px; border: 0px; padding: 0px;">${globalvar3}%</h1>`;
             let i = 0;
-            while (i !== (100 - parseInt(document.getElementById("globalvar3").innerHTML))) {
-                let positions = JSON.parse(document.getElementById("globalvar1").innerHTML);
+            while (i !== (100 - parseInt(globalvar3))) {
+                let positions = JSON.parse(globalvar1);
                 let levels = Object.keys(positions);
-                let colors = JSON.parse(document.getElementById("globalvar2").innerHTML);
+                let colors = JSON.parse(globalvar2);
                 let level = levels[[Math.floor(Math.random() * levels.length)]];
-                let usedLevelsDict = JSON.parse(document.getElementById("globalvar5").innerHTML);
+                let usedLevelsDict = JSON.parse(globalvar5);
                 if (Object.keys(usedLevelsDict).length === levels.length) {
                     usedLevelsDict = {};
                 }
@@ -169,10 +175,10 @@ function giveUp() {
                     level = levels[[Math.floor(Math.random() * levels.length)]];
                 }
                 usedLevelsDict[level] = positions[level];
-                document.getElementById("globalvar5").innerHTML = JSON.stringify(usedLevelsDict);
-                document.getElementById("globalvar4").innerHTML = colors[level.toLowerCase()];
+                globalvar5 = JSON.stringify(usedLevelsDict);
+                globalvar4 = colors[level.toLowerCase()];
                 document.getElementById("levels").innerHTML += `<div style="left: 50%; transform: translateX(-50%); border-radius: 25px; border: thick solid ${colors[level.toLowerCase()]}; text-align: center; width: 500px; bottom: 135px; position: relative;">
-                <h1 class="centered2" style="bottom: 15px; color: ${colors[level.toLowerCase()]};">#${positions[level]} - ${level}</h1><h1 style="color: ${colors[level.toLowerCase()]}; position: relative; bottom: 50px; font-family: 'Poppins', sans-serif; font-size: 50px; margin: 0px; border: 0px; padding: 0px;">${i + parseInt(document.getElementById("globalvar3").innerHTML) + 1}%</h1>
+                <h1 class="centered2" style="bottom: 15px; color: ${colors[level.toLowerCase()]};">#${positions[level]} - ${level}</h1><h1 style="color: ${colors[level.toLowerCase()]}; position: relative; bottom: 50px; font-family: 'Poppins', sans-serif; font-size: 50px; margin: 0px; border: 0px; padding: 0px;">${i + parseInt(globalvar3) + 1}%</h1>
                 </div>`
                 i++;
             }
--- dlv/script.js
+++ dlv/script.js
@@ -54,12 +54,28 @@ function hoverImg4(img) {
     img.style.minHeight = "300px";
 }
 
+function hoverImg5(img) {
+    img.src = "https://cornboar.com/cdn/packsbuttonhover.png";
+    img.style.maxHeight = "300px";
+    img.style.minHeight = "300px";
+}
+
 function unhoverImg4(img) {
     img.src = "https://cornboar.com/cdn/roulettebutton.png";
     img.style.maxHeight = "250px";
     img.style.minHeight = "250px";
 }
 
+function unhoverImg5(img) {
+    img.src = "https://cornboar.com/cdn/packsbutton.png";
+    img.style.maxHeight = "250px";
+    img.style.minHeight = "250px";
+}
+
 function redirect4() {
     window.location.href = "https://cornboar.com/dlv/roulette/";
+}
+
+function redirect5() {
+    window.location.href = "https://cornboar.com/dlv/packs/";
 }
