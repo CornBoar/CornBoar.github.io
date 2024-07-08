36b654f2f00ff578444588444e45750029708a5e
redx70 <64176323+redx70@users.noreply.github.com>
2024-07-08 15:07:30 -0700

    e

--- dlv/script.js
+++ dlv/script.js
@@ -1,15 +1,19 @@
 // alert("Justice For Niggetron!");
 
 function hoverImg(img) {
-    img.src = "https://cornboar.com/cdn/listbuttonhover.png";
+    img.src = "https://cornboar.com/assets/listbuttonhover.png";
     img.style.maxHeight = "300px";
     img.style.minHeight = "300px";
+    img.style.maxWidth = "650px";
+    img.style.minWidth = "650px";
 }
 
 function unhoverImg(img) {
-    img.src = "https://cornboar.com/cdn/listbutton.png";
+    img.src = "https://cornboar.com/assets/listbutton.png";
     img.style.maxHeight = "250px";
     img.style.minHeight = "250px";
+    img.style.maxWidth = "600px";
+    img.style.minWidth = "600px";
 }
 
 function redirect() {
@@ -17,15 +21,19 @@ function redirect() {
 }
 
 function hoverImg2(img) {
-    img.src = "https://cornboar.com/cdn/leaderboardbuttonhover.png";
+    img.src = "https://cornboar.com/assets/leaderboardbuttonhover.png";
     img.style.maxHeight = "300px";
     img.style.minHeight = "300px";
+    img.style.maxWidth = "650px";
+    img.style.minWidth = "650px";
 }
 
 function unhoverImg2(img) {
-    img.src = "https://cornboar.com/cdn/leaderboardbutton.png";
+    img.src = "https://cornboar.com/assets/leaderboardbutton.png";
     img.style.maxHeight = "250px";
     img.style.minHeight = "250px";
+    img.style.maxWidth = "600px";
+    img.style.minWidth = "600px";
 }
 
 function redirect2() {
@@ -33,15 +41,19 @@ function redirect2() {
 }
 
 function hoverImg3(img) {
-    img.src = "https://cornboar.com/cdn/accountbuttonhover.png";
+    img.src = "https://cornboar.com/assets/accountbuttonhover.png";
     img.style.maxHeight = "300px";
     img.style.minHeight = "300px";
+    img.style.maxWidth = "650px";
+    img.style.minWidth = "650px";
 }
 
 function unhoverImg3(img) {
-    img.src = "https://cornboar.com/cdn/accountbutton.png";
+    img.src = "https://cornboar.com/assets/accountbutton2.png";
     img.style.maxHeight = "250px";
     img.style.minHeight = "250px";
+    img.style.maxWidth = "600px";
+    img.style.minWidth = "600px";
 }
 
 function redirect3() {
@@ -49,27 +61,35 @@ function redirect3() {
 }
 
 function hoverImg4(img) {
-    img.src = "https://cornboar.com/cdn/roulettebuttonhover.png";
+    img.src = "https://cornboar.com/assets/roulettebuttonhover.png";
     img.style.maxHeight = "300px";
     img.style.minHeight = "300px";
+    img.style.maxWidth = "650px";
+    img.style.minWidth = "650px";
 }
 
 function hoverImg5(img) {
-    img.src = "https://cornboar.com/cdn/packsbuttonhover.png";
+    img.src = "https://cornboar.com/assets/packsbuttonhover.png";
     img.style.maxHeight = "300px";
     img.style.minHeight = "300px";
+    img.style.maxWidth = "650px";
+    img.style.minWidth = "650px";
 }
 
 function unhoverImg4(img) {
-    img.src = "https://cornboar.com/cdn/roulettebutton.png";
+    img.src = "https://cornboar.com/assets/roulettebutton2.png";
     img.style.maxHeight = "250px";
     img.style.minHeight = "250px";
+    img.style.maxWidth = "600px";
+    img.style.minWidth = "600px";
 }
 
 function unhoverImg5(img) {
-    img.src = "https://cornboar.com/cdn/packsbutton.png";
+    img.src = "https://cornboar.com/assets/packsbutton.png";
     img.style.maxHeight = "250px";
     img.style.minHeight = "250px";
+    img.style.maxWidth = "600px";
+    img.style.minWidth = "600px";
 }
 
 function redirect4() {
