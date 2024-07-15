6ce6e7588dbabf55dbd733ed3068e42f25d29655
redx70 <64176323+redx70@users.noreply.github.com>
2024-07-15 13:10:39 -0700

    Update script.js

--- bpm/script.js
+++ bpm/script.js
@@ -3,7 +3,6 @@ var clicks = 0;
 document.addEventListener("keypress", function onEvent(event) {
     if (event.key === "z" || event.key === "c") {
         clicks++;
-        document.getElementById("number").innerHTML = clicks * 60;
     }
 });
 
@@ -11,12 +10,11 @@ function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function loop() {
+async function calculateBpm() {
     while (true) {
-        await delay(1000);
-        clicks = 0;
-        document.getElementById("number").innerHTML = clicks;
+        await delay(5000);
+        document.getElementById("number").innerHTML = clicks * 12;
     }
 }
 
-loop();
+calculateBpm();
